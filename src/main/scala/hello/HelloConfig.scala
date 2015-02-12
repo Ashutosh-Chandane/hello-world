e34176bf404881e7b1973c6ba129a5f1b4d2fe46
Ashutosh Chandane <ashutoshchandane7390@gmail.com>
2015-02-11 19:52:06 -0800

    Changed HelloConfig.scala

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.{Configuration, ComponentScan}
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMethod
 
 @RestController
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 class HelloConfig {
-  @RequestMapping(Array("/hello"))
+  @RequestMapping(value=Array("/hello"), method=Array(RequestMethod.GET))
   def home() : String = {
     return "Hello World!";
   }
